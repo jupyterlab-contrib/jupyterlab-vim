38373d77896fc90c70e6130bdd3d3003257a2863
Axel Fahy <axelfahy@users.noreply.github.com>
2021-01-04 11:02:56 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup_args = dict(
     cmdclass= cmdclass,
     packages=setuptools.find_packages(),
     install_requires=[
-        "jupyterlab>=3.0.0rc2,==3.*",
+        "jupyterlab>=3.0.0,==3.*",
     ],
     zip_safe=False,
     include_package_data=True,
