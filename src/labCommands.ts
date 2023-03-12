ed56d27c03124c37b247b700879d196ec8ac9f10
Michał Krassowski <5832902+krassowski@users.noreply.github.com>
2023-03-12 22:45:59 +0000

    Lint

--- src/labCommands.ts
+++ src/labCommands.ts
@@ -144,25 +144,28 @@ export function addJLabCommands(
       isEnabled
     }),
     commands.addCommand('vim:leave-current-mode', {
-        label: 'Move Insert to Normal to Jupyter Command Mode"',
-        execute: args => {
-            const current = getCurrent(args);
+      label: 'Move Insert to Normal to Jupyter Command Mode"',
+      execute: args => {
+        const current = getCurrent(args);
 
-            if (current) {
-                const { content } = current;
-                if (content.activeCell !== null) {
-                    let editor = content.activeCell.editor as CodeMirrorEditor;
+        if (current) {
+          const { content } = current;
+          if (content.activeCell !== null) {
+            const editor = content.activeCell.editor as CodeMirrorEditor;
 
-                    // Get the current editor state
-                    if (editor.editor.state.vim.insertMode || editor.editor.state.vim.visualMode) {
-                      (CodeMirror as any).Vim.handleKey(editor.editor, '<Esc>');
-                    } else {
-                      commands.execute('notebook:enter-command-mode');
-                    }
-                }
+            // Get the current editor state
+            if (
+              editor.editor.state.vim.insertMode ||
+              editor.editor.state.vim.visualMode
+            ) {
+              (CodeMirror as any).Vim.handleKey(editor.editor, '<Esc>');
+            } else {
+              commands.execute('notebook:enter-command-mode');
             }
-        },
-        isEnabled
+          }
+        }
+      },
+      isEnabled
     }),
     commands.addCommand('vim:select-below-execute-markdown', {
       label: 'Execute Markdown and Select Cell Below',
